c6d9e8d842b7256494dfd6bab82fd0e26d873474
ceereenkoc <kocceereen@gmail.com>
2022-10-01 11:40:54 +0300

    Üçgen Çizme
    Kullanıcıdan alınan boyut bilgisine göre console'a Üçgen çizen uygulama

---
+++ üçgençizme/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ÜçgenÇizdir();
+        }
+
+        static void ÜçgenÇizdir()
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz:");
+            int input = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i <= input; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
