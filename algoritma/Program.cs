5fa8d72341c6f1dccb7c0d460402dc504489da7a
ceereenkoc <kocceereen@gmail.com>
2022-10-03 13:15:39 +0300

    Algoritma
    Ekrandan bir string bir de sayı alan (aralarında virgül ile), ilgili string ifade içerisinden verilen indexteki karakteri çıkartıp ekrana yazdıran console uygulamasıdır.

---
+++ algoritma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir kelime giriniz ve ardından çıkarmak istediğiniz karakterin indeksini giriniz(Aralarında virgül kullanınız.):");
+            string input = Console.ReadLine();
+
+            string[] input1 = input.Split(","); // Virgülü kaldırdık.
+            string kelime = input1[0]; //Kelime değişkenine dizinin virgülden önceki değeri atadık.
+            int çevirme = Convert.ToInt32(input1[1]); //Değerimiz string türündeydi, int türüne çevirdik.
+            string sonHal = kelime.Remove(çevirme, 1); //sonHal değişkenine sildiğimiz karakterli halini atadık.
+
+            Console.WriteLine(sonHal);
+        }
+    }
+}
