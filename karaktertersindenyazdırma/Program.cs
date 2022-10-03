d85be32303cd93cb3340e0c69c6d0cf9b595d84f
ceereenkoc <kocceereen@gmail.com>
2022-10-03 13:39:38 +0300

    Karakteri Tersten Yazdırma
    Verilen string ifade içerisindeki karakterleri bir önceki karakter ile yer değiştiren console uygulamasını yazınız.

---
+++ karaktertersindenyazdırma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace karakterterstenyazdırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Lütfen bir cümle giriniz:");
+            string cümle = Console.ReadLine();
+
+            char[] karakterler = cümle.ToCharArray(); // ToCharArray ile string ifadeki değerin karakterlere ayrılmasını sağladık ve bunu karakterler dizisine atadık.
+            string ters = String.Empty; //String.Empty'i tanımlanan string değerin boş bir değer tutması için kullandık.
+            for (int i = karakterler.Length - 1; i > -1; i--)
+            {
+                ters += karakterler[i];
+            }
+
+            Console.WriteLine(ters);
+
+        }
+    }
+}
